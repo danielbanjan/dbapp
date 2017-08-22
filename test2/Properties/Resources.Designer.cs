902ea4e8b8506064e7ae6b5059d51ca75df2ea36
Banjan Daniel <danielbanjan@yahoo.com>
2017-08-22 09:42:47 +0300

    lots of stuff

--- test2/Properties/Resources.Designer.cs
+++ test2/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace test2.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon logo_icon_600x315_II0_icon {
+            get {
+                object obj = ResourceManager.GetObject("logo_icon_600x315_II0_icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
